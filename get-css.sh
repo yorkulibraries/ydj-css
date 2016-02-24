d3e6e690e653b1a4e25cc43bb28f34de7f32e9a9
nina de jesus <satifice@riseup.net>
2016-02-24 11:25:24 -0500

    fix hssh header problem

--- get-css.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#JOURNAL=witness currents tusaaji topia theoriandpraxis sclr soi hssh refuge protem pivot ngn lh jarm jppal jid ijcst edu_hse-rhe yujhs historicalpapers ht gjis cws crsp caml
-
-while read line
-do
-    wget -i $line 
-done < urls.txt
