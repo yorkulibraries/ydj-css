473b720d46ea1255fac095d9a0e922dbcc9eeb24
nina de jesus <satifice@riseup.net>
2017-04-22 11:34:59 -0400

    just labour is finished

--- justlabour-child-theme/justlabourThemePlugin.inc.php
+++ justlabour-child-theme/justlabourThemePlugin.inc.php
@@ -6,7 +6,7 @@ class justlabourThemePlugin extends ThemePlugin {
   
   public function init() {
     $this->setParent('defaultthemeplugin');
-    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less')));
+    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/custom.less')));
   }
   
   public function getDisplayName() {
