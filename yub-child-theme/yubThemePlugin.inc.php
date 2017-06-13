d75f0be645ddf4b963826a249693527ce89bd426
nina de jesus <satifice@riseup.net>
2017-06-13 12:18:22 -0400

    working on yub

--- yub-child-theme/yubThemePlugin.inc.php
+++ yub-child-theme/yubThemePlugin.inc.php
@@ -2,11 +2,9 @@
 
 import('lib.pkp.classes.plugins.ThemePlugin');
 
-class yubThemePlugin extends ThemePlugin {
-  
   public function init() {
     $this->setParent('defaultthemeplugin');
-    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less')));
+    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less','styles/no-submissions.less')));
   }
   
   public function getDisplayName() {
@@ -17,5 +15,4 @@ class yubThemePlugin extends ThemePlugin {
     return 'This is a custom theme developed by the digital projects team at York University Libraries.';
   }
 }
-
 ?> 
