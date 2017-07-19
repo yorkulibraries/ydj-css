6eaa25b419e843f7bdeeadad62e09f6a57843604
nina de jesus <satifice@riseup.net>
2017-07-19 11:45:53 -0400

    done

--- hpsj-child-theme/hpsjThemePlugin.inc.php
+++ hpsj-child-theme/hpsjThemePlugin.inc.php
@@ -6,7 +6,7 @@ class hpsjThemePlugin extends ThemePlugin {
   
   public function init() {
     $this->setParent('defaultthemeplugin');
-    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less','styles/sidebar.less')));
+    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less')));
   }
   
   public function getDisplayName() {
