bc3a0756f0a86d1cc3e96a8c91259221bd8fb412
nina de jesus <satifice@riseup.net>
2017-07-26 10:48:23 -0400

    etopia finished

--- etopia-child-theme/etopiaThemePlugin.inc.php
+++ etopia-child-theme/etopiaThemePlugin.inc.php
@@ -6,7 +6,7 @@ class etopiaThemePlugin extends ThemePlugin {
   
   public function init() {
     $this->setParent('defaultthemeplugin');
-    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less','styles/sidebar.less')));
+    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/custom.less')));
   }
   
   public function getDisplayName() {
