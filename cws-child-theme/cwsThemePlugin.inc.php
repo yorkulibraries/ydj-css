cfd3eda504bd4a1a9721319fe903bb3e0ce4fe1a
nina de jesus <satifice@riseup.net>
2017-08-02 12:00:11 -0400

    cleaning up

--- cws-child-theme/cwsThemePlugin.inc.php
+++ cws-child-theme/cwsThemePlugin.inc.php
@@ -6,7 +6,7 @@ class cwsThemePlugin extends ThemePlugin {
   
   public function init() {
     $this->setParent('defaultthemeplugin');
-    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less','styles/no-submissions.less')));
+    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/header-image.less','styles/custom.less')));
   }
   
   public function getDisplayName() {
